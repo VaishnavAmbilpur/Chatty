3da7e1a3b256bd88f8a805a51bfb324f766e35d7
VaishnavAmbilpur <vaishnavambilpur2006@gmail.com>
2025-08-25 23:51:26 +0530

    fist

--- Real-Time-Chat FrontEnd/src/Components/Chat.tsx
+++ Real-Time-Chat FrontEnd/src/Components/Chat.tsx
@@ -51,11 +51,32 @@ const Chat = () => {
  useEffect(()=>{
    scrollToBottomDiv();
  },[messages])
+ function sendMessage(){
+    //@ts-ignore
+    const message = inputRef.current?.value;
+    console.log(message);
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+    //@ts-ignore
+    wsRef.current.send(JSON.stringify({
+      type: "chat",
+      payload: {
+        message: message
+      }
+    }))
+    scrollToBottomDiv();
+ }
+ function handleKeyDown(event:any) {
+    if (event.key === 'Enter') {
+       sendMessage();
+    }
+  }
   return (
     <div className='font-Josefin bg-zinc-950 border-1 border-zinc-700 my-auto min-w-auto max-h-auto md:min-w-auto md:max-h-auto overflow-hidden rounded-md text-white m-10 overflow-x-hidden overflow-y-hidden'>
         <div className='text-3xl tracking-tighter p-4 font-semibold flex items-start flex-wrap gap-x-4 gap-y-2 flex-col'>
          <div className='flex items-center gap-x-3'><ChatIcon/>
-          Real-Time-Chat</div> 
+          Chatty</div> 
           <div className='text-sm text-zinc-600 tracking-normal'>Temperory Chat Room which is deleted after you left</div>
         </div>
         <div className='m-3 font-Josefin hover:bg-zinc-700 rounded-md p-2 text-zinc-500 font-bold flex justify-between bg-zinc-800'>
@@ -64,10 +85,10 @@ const Chat = () => {
         <div className='p-2 rounded-lg'>
           <div ref={mesRef} className='h-[400px] flex flex-col gap-y-0.5 w-[350px] overflow-x-hidden overflow-y-scroll'>
        {/* Display all messages */}
-       {messages.map(message => <div className='m-2 w-fit flex flex-col'> 
-         <span className='bg-white text-black rounded f p-4 '>            
-           {message} 
-         </span>
+       {messages.map(message => <div className='m-2 w-fit flex flex-col'>  
+    <span className='bg-white inline-block text-black rounded max-w-xs break-words p-4'>            
+  {message} 
+</span>
        </div>)}
      </div>
           </div>
@@ -75,21 +96,8 @@ const Chat = () => {
             <input placeholder='Enter Room code' className=' rounded-md h-fit focus:border-white placeholder-zinc-600 w-62 p-3 border-1 border-zinc-700 ' ref={inputRef}></input>
            <button className=' w-full  items-center font-Josefin h-fit hover:bg-slate-100 rounded-md p-3 text-zinc-950 font-bold flex justify-evenly bg-white cursor-pointer' onClick={()=>{
               // @ts-ignore
-            const message = inputRef.current?.value;
-            console.log(message);
-             if (inputRef.current) {
-                  inputRef.current.value = '';
-              }
-            // @ts-ignore
-            wsRef.current.send(JSON.stringify({
-               type: "chat",
-           payload: {
-             message: message
-           }
-           
-           }))
-           scrollToBottomDiv();
-           }}>
+              sendMessage();
+           }} onKeyDown={(e) => handleKeyDown(e)}>
              Send</button> 
         </div>
     </div>
